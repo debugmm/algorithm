7f3ecf6ab71f451eca22a932296b88407ab9aa38
wujungao <abuglife@qq.com>
2018-11-16 15:04:01 +0800

    c find max sub continuous string from string

---
+++ find_max_subContinuousString_fromString.c
@@ -0,0 +1,27 @@
+int findMaxSubContinousString(char array){
+	
+	int maxLength=0;
+	int position=0;
+	int length=1;
+	
+	int stringLength=strlen(array);
+	for(i=1;i<stringLength;i++){
+		if(array[i]==array[i-1]+1){
+			
+			length++;
+		}
+		else{
+			
+			if(length>max){
+				maxLength=length;
+			}
+			length=1;
+			position=i-maxLength;
+		}
+	}
+	
+	for(int j=position;j<postion+maxLength;j++){
+		
+		printf("%s",array[j]);
+	}
+}
